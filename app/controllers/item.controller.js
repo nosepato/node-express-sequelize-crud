31b465c121fe30d5c84df47eea51b5c48260f845
nosepato <felipe.pinhoramos@outlook.com>
2023-08-28 15:46:50 -0300

    alimentando o controller

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -3,11 +3,47 @@ const Items = db.item;
 const Op = db.Sequelie.Op;
 
 exports.create = (req, res) => {
+    if (!req.body.name) {
+        res.status(400).send({
+            message: "Tá vazio, meu camarda, preenche aí!"
+        });
+        return;
+    }
 
+    const item = {
+        name: req.body.name,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        is_flammable: req.bady.is_flammable ? req.body.is_flammable: false
+    }
+
+    Items.create(item)
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Deu b.o na criação do item."
+        })
+    })
 };
 
-exports.findAll = (req, res) => {
 
+exports.findAll = (req, res) => {
+    const name = req.body.name;
+    var condition = name ? { name: { [Op.like]: `%${name}%`} } :null;
+
+    items.findAll({where: condition})
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "Deu b.o na listagem dos item."
+        })
+    })
 };
 
 exports.findOne = (req, res) => {
